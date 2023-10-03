26754ad422b79812c2fdc796c56d0040eef715c2
Drev07 <andrewsalazar629@gmail.com>
2023-10-02 22:32:13 -0600

    fix("factorial")

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -8,31 +8,29 @@ using namespace std;
  double factorial(int n, int r){
      double  f=1.0, fr=1.0;
      long totalfinal = 0, total = 0, tf =1;
+     /*factorial n*/
     for(i=n; i>=1; i--){
-        f *=i;
-        //cout<<"count "<<f<<endl;     
+        f *=i;   
     }
-    cout<<"count "<<f<<endl;
-     for(int a=r; a>=1; a--){
-        fr *=a;
-        //cout<<"count "<<f<<endl;     
+    cout<<"datos de n "<<f<<endl;
+      /*factorial r*/
+     /*for(int a=r; a>=1; a--){
+        fr *=a;    
         }
-        cout<<"count "<<fr<<endl;
-        total += fr-f;
-        fn= abs(total);
+        cout<<"datos de r "<<fr<<endl;*/
+        total += n-r;
+        /*el abs es para eliminar los signos negativos de la respuesta*/
+       fn= abs(total);
        
     
-        cout<<"total  fn"<<fn<<endl;
-       
-
-        for(int b=fn; b> fn -3; b--){
-            tf *= b;
-            
-            cout<<"factorial de la resta fr*f:"<<tf<<endl;  
-               
+        cout<<"total   de resta   "<<fn<<endl;
+          /*factorial n-r*/
+        for(int b= total ; b>=1; b--){
+            tf*= b;   
         }
-        totalfinal =tf/fn;
-        cout<<"total final tf/fn "<<totalfinal <<endl;
+        cout<<"factorial de la resta n-r:"<<tf<<endl;  
+        totalfinal =f/fn;
+        cout<<"total del factorial "<<totalfinal<<endl;
         return totalfinal;
 }
 
@@ -43,7 +41,6 @@ int main(){
     cin>>r;
      cout<<"si llega ";
     double resufactorial = factorial(n, r);
-    cout<<"si llega 11"<<endl;
    cout<<""<<resufactorial;
 
 
