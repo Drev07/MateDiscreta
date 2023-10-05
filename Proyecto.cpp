4cc743533af35e9f8ab28f969811e01ef9757f2a
Drev07 <andrewsalazar629@gmail.com>
2023-10-04 23:10:38 -0600

    correir error de calculo

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -3,33 +3,31 @@
 #include<string>
 
 using namespace std;
- int r, n,  i, fn=1; 
+ int r, n,  i, fn=0; 
 
  double factorial(int n, int r){
      double  f=1.0, fr=1.0;
-     long totalfinal = 0, total = 0, tf =1;
+     long totalfinal = 0, total = 0, tf =0;
      /*factorial n*/
     for(i=n; i>=1; i--){
         f *=i;   
     }
     cout<<"datos de n "<<f<<endl;
-      /*factorial r*/
-     /*for(int a=r; a>=1; a--){
-        fr *=a;    
-        }
-        cout<<"datos de r "<<fr<<endl;*/
-        total += n-r;
+        total = n-r;
         /*el abs es para eliminar los signos negativos de la respuesta*/
-       fn= abs(total);
-       
+       fn += abs(total);
+       cout<<"total: "<<total;
     
         cout<<"total   de resta   "<<fn<<endl;
           /*factorial n-r*/
         for(int b= total ; b>=1; b--){
             tf*= b;   
+            cout<<"coutor r: "<<b;
         }
         cout<<"factorial de la resta n-r:"<<tf<<endl;  
-        totalfinal =f/fn;
+        cout<<"factorial n: "<<f<<endl;
+        cout<<"factorial r: "<<tf<<endl;
+        totalfinal = f/tf;
         cout<<"total del factorial "<<totalfinal<<endl;
         return totalfinal;
 }
@@ -41,7 +39,7 @@ int main(){
     cin>>r;
      cout<<"si llega ";
     double resufactorial = factorial(n, r);
-   cout<<""<<resufactorial;
+   cout<<""<<resufactorial<<endl;
 
 
     return 0;
